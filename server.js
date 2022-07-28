4e98b4548d4d346b71de37c730e7804455060a0d
Viry <diana13_2011@hotmail.com>
2022-07-28 09:41:25 -0500

    connected to heroku

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(cors())
 app.use(express.json())
 app.use(logger('dev'))
 app.use(express.urlencoded({ extended: false }))
+app.use(express.static(`${__dirname}/client/build`))
+
 
 app.get('/categories', categoryController.getAllCategories)
 app.get('/categories/:id/posts', postController.getPostsByCat)
@@ -27,4 +29,7 @@ app.delete('/categories/:id/posts/:id', postController.deletePost)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+  })
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
