d35cc08a0edaaf9de22748ad0ef4fb73cf3210f9
Viry <diana13_2011@hotmail.com>
2022-07-28 16:11:30 -0500

    made nav bar a bit cooler

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
 
   return(
-    <nav className='navbar'>
+    <nav className='navbar borderXwidth'>
       <h4>Tattoo's Blog </h4>
       <Link to="/">Home</Link>
       <Link to='/categories'>Categories</Link>
